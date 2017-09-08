887b09c06167a58fba5ab04eab2422df11d79735
Lee Becker <lee.becker@gmail.com>
2017-09-08 03:05:12 -0600

    Documenting initial dev tool setup for OS X.

---
+++ osx_setup/install.sh
@@ -0,0 +1,19 @@
+# install brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install Neovim
+brew install nvim
+
+# install VimR
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+and press enter/return key. Wait for the command to finish.
+Run:
+brew cask install vimr
+
+# install git
+brew install git
+
+# install gitx
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+brew cask install gitx
+
